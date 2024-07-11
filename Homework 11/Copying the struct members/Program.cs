ff46a020cd9b1a9a4803f6e53b636ba7631b7674
Sergey <79244748+Hambar17umyan@users.noreply.github.com>
2024-07-11 16:02:39 +0400

    Struct members copying

---
+++ Homework 11/Copying the struct members/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Copying_the_struct_members
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    struct MyStruct
+    {
+        public int i;
+        public string s;
+        public int[] arr;
+
+        public void Copy(MyStruct destinationMyStruct)
+        {
+            destinationMyStruct.i = i;
+
+            destinationMyStruct.s = new string(s);
+            Array.Copy(arr, destinationMyStruct.arr, arr.Length);
+        }
+    }
+
+}
